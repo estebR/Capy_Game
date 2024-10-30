99cb812fcbec7845d7b915fc0898bebdb04acc76
estebR <149003565+estebR@users.noreply.github.com>
2024-10-30 17:13:43 -0400

    Delete Capybara.js

--- Capybara.js
+++
@@ -1,8 +0,0 @@
-//Use of queryselector for easy to use DomContent
-const canvas = document.querySelector("myCanvas");
-const score= document.querySelector("score")
-const ctx= canvas.getContext("2d");
-
-// The width and height of the screen
-canvas.width = innerWidth;
-canavs.height= innerHeight;
