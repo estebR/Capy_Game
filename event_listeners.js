a1663935a9f52431e47947ba9642e09995d7b0f3
estebR <Srodriguex123@icloud.com>
2024-10-30 12:15:13 -0400

    Added files from Capybara directory

---
+++ event_listeners.js
@@ -0,0 +1,38 @@
+//Function to call in main.js
+function elementIDSListener(game){
+    const gameStart=document .querySelector("#gameStart")
+    const gameEnd=document .querySelector("#gameEnd")
+    const start_button=document .querySelector("#start")
+    const leaderboard_button=document .querySelector("#leaderboard_button")
+    const custom_button=document .querySelector("#custom")
+    const win_lose=document .querySelector("#gameWinLoseSpan")
+    const score=document .querySelector("#gameEndScoreSpan")
+    const leaderboard=document.querySelector("#leaderboard");
+    const exit_leaderboard=document.querySelector("#exit_leaderboard")
+
+   
+    start_button.addEventListener("click",()=>{
+        gameStart.style.display="none"
+        game.scene.resume("scene-game")
+    })
+    
+    leaderboard_button.addEventListener("click",()=>{
+        console.log("Leaderboard button clicked");
+        gameStart.style.display="none";
+        leaderboard.style.display="block";
+
+        
+        })
+    exit_leaderboard.addEventListener("click",()=>{
+        
+        leaderboard.style.display="none";
+        gameStart.style.display="";
+    })
+    custom_button.addEventListener("click"),()=>{
+        
+    }
+    
+
+}
+
+export default elementIDSListener;
---
+++ player.js
@@ -0,0 +1,10 @@
+class Player {
+    constructor(){
+
+        //Player speed
+        this.velocity= {
+            x:0,
+            y:0
+        }
+    }
+}
