146f040791d74d570438813022d84e30eea05868
estebR <Srodriguex123@icloud.com>
2024-11-01 15:04:31 +0000

    Add main.js, app.js, and public folder

---
+++ app.js
@@ -0,0 +1,63 @@
+const http = require('http');
+const fs= require('fs');
+const url= require('url');
+const path= require('path');
+sendFile = function (reqObj, resObj) {
+        let fileName = url.parse(reqObj.url,true);
+        console.log(reqObj.url);
+	//We add a parameter for the object
+	if (fileName.pathname==="/"){
+		fileName.pathname="/index.html"
+	}
+	else {
+		fileName.pathname=fileName.pathname
+	}
+	console.log(fileName.pathname);
+        fs.readFile("."+fileName.pathname,function(err, data){
+		readData(err,data,fileName,resObj);
+	});
+};
+//Function to get all types of file in different directories
+function getContentType(x){
+
+	switch(path.extname(x)){
+
+	case ".html":
+		return "text/html";
+
+	case ".jpg":
+		return "image/jpeg";
+	case ".png":
+		return "image/png";
+	case ".wav":
+		return "audio/wav";
+
+	case ".mp3":
+		return "audio/mpeg";
+
+	case ".css":
+		return "text/css";
+
+	case ".js":
+		return "text/javascript";
+
+	case ".txt":
+		return "text/plain";
+	}
+}
+//Function to output the dat inside the file we are using
+function readData(err,data,fileName,resObj){
+	if (err) {
+                        resObj.writeHead(404, {"Content-Type":"text/plain"});
+                        resObj.write("404 not found");
+                        resObj.end();
+                }
+        else{
+                        resObj.writeHead(200,{"Content-Type":getContentType(fileName.pathname)});
+                        resObj.write(data);
+                        resObj.end();
+
+                }
+};
+const myserver = http.createServer(sendFile); //create a server object
+myserver.listen(80, function() {console.log("Listening on port 80" )});
