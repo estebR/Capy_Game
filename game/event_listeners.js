9be50fbe34709b6e64cc70227ad09fa6825b5305
Hamza Nazim <hamzanazim81@gmail.com>
2024-11-18 15:02:15 -0500

    Gameover
    fixing Gameover function

--- game/event_listeners.js
+++ game/event_listeners.js
@@ -1,14 +1,14 @@
 //Function to call in main.js
 function elementIDSListener(game){
-    const gameStart=document .querySelector("#gameStart")
-    const gameEnd=document .querySelector("#gameEnd")
-    const start_button=document .querySelector("#start")
-    const leaderboard_button=document .querySelector("#leaderboard_button")
-    const custom_button=document .querySelector("#custom")
-    const win_lose=document .querySelector("#gameWinLoseSpan")
-    const score=document .querySelector("#gameEndScoreSpan")
-    const leaderboard=document.querySelector("#leaderboard");
-    const exit_leaderboard=document.querySelector("#exit_leaderboard")
+    const gameStart = document.querySelector("#gameStart")
+    const gameEnd = document.querySelector("#gameEnd")
+    const start_button = document.querySelector("#start")
+    const leaderboard_button = document.querySelector("#leaderboard_button")
+    const custom_button = document.querySelector("#custom")
+    const win_lose = document.querySelector("#gameWinLoseSpan")
+    const score = document.querySelector("#gameEndScoreSpan")
+    const leaderboard = document.querySelector("#leaderboard");
+    const exit_leaderboard = document.querySelector("#exit_leaderboard")
 
    
     start_button.addEventListener("click",()=>{
